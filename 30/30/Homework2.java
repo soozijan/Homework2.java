13e64b9d34dfdb47dda83574406c66c0a8267ae3
soozijan <75090137+soozijan@users.noreply.github.com>
2020-12-04 15:18:24 +0200

    Create Homework2.java

---
+++ 30/30/Homework2.java
@@ -0,0 +1,13 @@
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter account owner name: ");
+        String my_name = scanner.nextLine();// why I have to write this?
+        int my_saving = 0;
+        my_saving +=10_000;
+        my_saving *= 2;
+        my_saving += 30_000;
+        my_saving ++;
+        my_saving /= 2;
+        System.out.println("owner; " + my_name + "Balance: " + my_saving);
+        
+        
+        
